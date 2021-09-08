dcd974ea61ff368b85b84b4bacd0426fdca52032
pavankrishnanrit <pavankrishnanr.it2020@citchennai.net>
2021-09-08 14:12:44 +0530

    BMI CALCULATOR

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kLabelTextStyle = TextStyle(fontSize: 18, color: Color(0xFF8D8E98));
+const kNumberTextStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.w900);
+const double kBottomContainerHeight = 80;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFF111328);
+const kBottomContainerColour = Color(0xFFEB1555);
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.bold,
+);
+const kTitleTextStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 50);
+const kResultTextStyle =
+    TextStyle(color: Colors.green, fontSize: 45, fontWeight: FontWeight.bold);
+const kBMITextStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 100);
+const kBodyTextStyle = TextStyle(fontSize: 22);
